67b4beb99bc9ea4e9b459d04c96405f20ef715e2
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-22 09:43:17 -0300

    feat: config inicial estilos

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
