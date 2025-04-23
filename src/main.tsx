0db817dc235fbee1b830255c65606eb324ffea23
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-23 07:57:30 -0300

    feat: add seção de perfil

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
-import { Header } from './components/Header';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Header />
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
