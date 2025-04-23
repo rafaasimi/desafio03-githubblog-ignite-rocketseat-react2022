9c2bb732d66017d9b682b0619b465f8d9a249404
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-23 08:27:46 -0300

    feat: add lista de postagens

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,12 +1,128 @@
 import { Profile } from "./components/Profile";
 import { SearchForm } from "./components/SearchForm";
-import { PostsContainer } from "./styles";
+import { PostItem, PostList, PostsContainer } from "./styles";
 
 export function Posts() {
   return (
     <PostsContainer>
       <Profile />
       <SearchForm />
+
+      <PostList>
+        <PostItem>
+          <a href="www.google.com">
+            <header>
+              <h2>JavaScript data types and data structures</h2>
+              <time title="23/04/2025">Há 1 dia</time>
+            </header>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic.
+            </p>
+          </a>
+        </PostItem>
+
+        <PostItem>
+          <a href="www.google.com">
+            <header>
+              <h2>JavaScript data types and data structures</h2>
+              <time title="23/04/2025">Há 1 dia</time>
+            </header>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic.
+            </p>
+          </a>
+        </PostItem>
+
+        <PostItem>
+          <a href="www.google.com">
+            <header>
+              <h2>JavaScript data types and data structures</h2>
+              <time title="23/04/2025">Há 1 dia</time>
+            </header>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic.
+            </p>
+          </a>
+        </PostItem>
+
+        <PostItem>
+          <a href="www.google.com">
+            <header>
+              <h2>JavaScript data types and data structures</h2>
+              <time title="23/04/2025">Há 1 dia</time>
+            </header>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic.
+            </p>
+          </a>
+        </PostItem>
+
+        <PostItem>
+          <a href="www.google.com">
+            <header>
+              <h2>JavaScript data types and data structures</h2>
+              <time title="23/04/2025">Há 1 dia</time>
+            </header>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic.
+            </p>
+          </a>
+        </PostItem>
+
+        <PostItem>
+          <a href="www.google.com">
+            <header>
+              <h2>JavaScript data types and data structures</h2>
+              <time title="23/04/2025">Há 1 dia</time>
+            </header>
+
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn. Dynamic typing JavaScript is a loosely typed and
+              dynamic.
+            </p>
+          </a>
+        </PostItem>
+      </PostList>
     </PostsContainer>
-  )
+  );
 }
--- src/pages/Posts/styles.ts
+++ src/pages/Posts/styles.ts
@@ -3,8 +3,64 @@ import styled from "styled-components";
 export const PostsContainer = styled.div`
   position: relative;
   top: -5.5rem;
-  
+
   max-width: 54rem;
   margin: 0 auto;
   padding: 0 1rem;
+`;
+
+export const PostList = styled.main`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+
+  @media (width < 768px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const PostItem = styled.article`
+  a {
+    padding: 2rem;
+    border-radius: 10px;
+    background: ${(props) => props.theme["base-post"]};
+
+    display: flex;
+    flex-direction: column;
+    gap: 1.25rem;
+
+    border: 2px solid transparent;
+
+    &:hover {
+      border: 2px solid ${(props) => props.theme["base-label"]};
+    }
+
+    header {
+      display: flex;
+      align-items: flex-start;
+      justify-content: space-between;
+      gap: 1rem;
+
+      h2 {
+        font-size: 1.25rem;
+        line-height: 160%;
+        color: ${(props) => props.theme["base-title"]};
+      }
+
+      time {
+        font-size: 0.875rem;
+        line-height: 160%;
+        color: ${(props) => props.theme["base-span"]};
+        white-space: nowrap;
+      }
+    }
+
+    p {
+      display: -webkit-box;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
 `;
